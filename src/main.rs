50fa5729d59e715355495c7e42d0f1059cf1b575
0elek <87550550+0elek@users.noreply.github.com>
2023-07-05 03:35:19 +0200

    reset

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use rand::Rng;
+use std::time::{Duration, Instant};
+use std::thread::sleep;
+
+const WIDTH: usize = 10;
+const HEIGHT: usize = 10;
+
+#[derive(Debug, Clone, Copy)]
+struct Sandbox {
+    cells: [[bool; WIDTH]; HEIGHT],
+}
+
+
+fn main(){
+
+
+todo!();
+
+}
